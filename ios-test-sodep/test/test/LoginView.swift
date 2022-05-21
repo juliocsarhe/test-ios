e08b17a77a4f3889abc6bb57dce1646564ee1bfb
juliocsarhe <juliocsarhe@gmail.com>
2022-05-21 00:12:28 -0400

    test
    Diseño básico del login

--- ios-test-sodep/test/test/LoginView.swift
+++ ios-test-sodep/test/test/LoginView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var username: String = "username"
+    @State private var password: String = "password"
     var body: some View {
-        Image("loginIcon")
-                    .clipShape(Circle())
-                    .overlay {
-                        Circle().stroke(.gray, lineWidth: 4)
-                    }
-                    .shadow(radius: 7)
+        VStack{
+            Image("loginIcon").resizable()
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle().stroke(.gray, lineWidth: 4)
+                        }.frame(width: 150, height: 150, alignment: .center)
+                .shadow(radius: 7).padding(30)
+            Spacer()
+            VStack {
+                TextField("username", text: $username).cornerRadius(5.0).multilineTextAlignment(.center)
+                Divider()
+                 .frame(height: 1)
+                 .padding(.horizontal, 30)
+                 .background(.black)
+                 .padding(.bottom, 15)
+                SecureField("password", text: $password).cornerRadius(5.0).multilineTextAlignment(.center)
+                Divider()
+                 .frame(height: 1)
+                 .padding(.horizontal, 30)
+                 .background(.black)
+                 .padding(.bottom, 30)
+                Button("Sign in"){
+                    
+                }
+            }.padding(.horizontal, 72)
+            
+            Spacer(minLength: 60)
+            Button("Create a new account"){
+                
+            }.padding()
+            Button("Forgot my password"){
+                
+            }.padding()
+        }
+        
+        
     }
 }
 
